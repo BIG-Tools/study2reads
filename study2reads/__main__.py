4e52adcb6f3f1f54faeeebb5a5e009bdb986d333
Pierre Marijon <pierre@marijon.fr>
2016-01-25 17:18:03 +0100

    We can call main function of package if we have dict with parametre

--- study2reads/__main__.py
+++ study2reads/__main__.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+"""
+Is just main file of study2reads function
+"""
+
 # std import
 import sys
 
@@ -7,6 +11,11 @@ import sys
 from . import cli
 from . import ena
 
-arg = cli.parser.read_arg(sys.argv[1:])
+def main(arg):
+    """ Main function of study2reads function """
+    ena.get.reads(**arg)
 
-ena.get.reads(**arg)
+if __name__ == "__main__":
+
+    main(cli.parser.read_arg(sys.argv[1:]))
+
